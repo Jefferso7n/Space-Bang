8b6b695376515a37fcbe99592f8f7b1720460e6c
Jefferso7n <jeffersonfillipe2021@gmail.com>
2022-08-21 23:47:41 -0300

    Initial scripts
    Player movement, shooting and enemies AI scripts

---
+++ Abyssal Sea LFS/Assets/Scripts/Enemy Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float spawnRadius = 7, time = 1.5f;
+
+    public GameObject[] enemies;
+
+    void Start()
+    {
+        StartCoroutine(SpawnAnEnemy());
+    }
+
+    IEnumerator SpawnAnEnemy()
+    {
+        Vector2 spawnPosition = GameObject.Find("Player").transform.position;
+        spawnPosition += Random.insideUnitCircle.normalized * spawnRadius;
+
+        Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPosition, Quaternion.identity);
+        yield return new WaitForSeconds(time);
+        StartCoroutine(SpawnAnEnemy());
+    }
+}
