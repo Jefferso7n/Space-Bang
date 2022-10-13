3e9dc13aa17b800830dce9523d39fd84e80de147
Jefferso7n <jeffersonfillipe2021@gmail.com>
2022-10-13 13:15:36 -0300

    Change in Player/Enemy movement
    The player now has an initial acceleration. It takes a while to reach maximum speed.
    Enemy movement is now done with MovePosition(). It is now more difficult to have groups of enemies.

--- Space Bang - LFS/Assets/Scripts/Player/PlayerSpeed.cs
+++ Space Bang - LFS/Assets/Scripts/Player/PlayerSpeed.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class PlayerSpeed : MonoBehaviour
 {
     #region Declarations
-    [SerializeField] private float maxSpeed;
-    public float currentSpeed { get; private set; }
+    [SerializeField] float speed = 2f;
+    [SerializeField] float duration = 1.5f;
+    [SerializeField] AnimationCurve speedCurve;
+    [SerializeField] PlayerController playerController;
+
+    private float currentTime = 0f;
     #endregion
 
     #region Speed
-    void Awake(){
-        currentSpeed = maxSpeed;
-    }
-
-    public void UpdateSpeed(float mod)
+    public float GetSpeed()
     {
-        currentSpeed += mod;
+        return speed * speedCurve.Evaluate(TimeManagement(playerController.isMoving));
     }
     #endregion
 
+    #region Time
+    private float TimeManagement(bool isMoving)
+    {
+        if (isMoving)
+        {
+            currentTime += Time.deltaTime;
+
+            if (currentTime >= duration)
+            {
+                currentTime = duration;
+            }
+        }
+        else
+        {
+            currentTime -= Time.deltaTime;
+
+            if (currentTime <= 0f)
+            {
+                currentTime = 0f;
+            }
+
+        }
+
+        return currentTime / duration;
+    }
+    #endregion
 }
