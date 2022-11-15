41c71fad1140c515de65bed369ece40bad76c7cb
Vitoria <vii.rmoura@gmail.com>
2022-11-15 18:00:05 -0300

    Merge branch 'main' into Animações
    # Conflicts:
    #	Space Bang - LFS/Assets/Prefabs/Enemies/Enemy.prefab
    #	Space Bang - LFS/Assets/Scenes/Game.unity
    #	Space Bang - LFS/Assets/Scripts/Enemy/EnemyFollow.cs
    #	Space Bang - LFS/Assets/Scripts/Enemy/EnemyHealth.cs
    #	Space Bang - LFS/Assets/Sprites/Alien.png.meta
    #	Space Bang - LFS/Assets/Sprites/Animations/Alien/Enemy.controller
    #	Space Bang - LFS/UserSettings/Layouts/default-2021.dwlt

--- Space Bang - LFS/Assets/Scripts/Damage Dealer/DamageDealer.cs
+++ Space Bang - LFS/Assets/Scripts/Damage Dealer/DamageDealer.cs
@@ -6,11 +6,9 @@ public class DamageDealer : MonoBehaviour
 {
     // Responsible for managing enemy and player damage
     [SerializeField] int damage = 10;
-    [SerializeField] int damageVariance = 3;
 
     public int GetDamage(){
-//        return damage;
-        return Random.Range(damage - damageVariance, damage + damageVariance);
+        return damage;
     }
 
     // public void Hit(){
